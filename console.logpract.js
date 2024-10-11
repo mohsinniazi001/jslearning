2f076d056f46a3eb851c11a659ea5d498ceb9910
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-10-11 13:11:28 +0500

    separate console examples in new file

---
+++ console.logpract.js
@@ -0,0 +1,81 @@
+console.log("Practice questions using only console.log()");
+
+console.log("Use console.log() to draw a 5x5 square of * symbols.");
+
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+
+console.log("Use console.log() to draw a right-angled triangle made of *.");
+
+console.log("*");
+console.log("**");
+console.log("* *");
+console.log("*  *");
+console.log("*   *");
+console.log("*    *");
+console.log("*     *");
+console.log("*      *");
+console.log("*********");
+
+console.log("Use console.log() to draw an inverted triangle.");
+
+console.log("*********");
+console.log("*      *");
+console.log("*     *");
+console.log("*    *");
+console.log("*   *");
+console.log("*  *");
+console.log("* *");
+console.log("**");
+console.log("*");
+
+console.log("Create a pyramid using * symbols with 5 levels.");
+
+console.log("    *    ");
+console.log("   ***   ");
+console.log("  *****  ");
+console.log(" ******* ");
+console.log("*********");
+
+console.log("Use console.log() to draw a diamond shape.");
+
+console.log("    *    ");
+console.log("   ***   ");
+console.log("  *****  ");
+console.log(" ******* ");
+console.log("*********");
+console.log(" ******* ");
+console.log("  *****  ");
+console.log("   ***   ");
+console.log("    *    ");
+
+console.log("Use console.log() to draw a 5x5 hollow square.");
+
+console.log("*****");
+console.log("*   *");
+console.log("*   *");
+console.log("*   *");
+console.log("*****");
+
+console.log("Use console.log() to draw a hollow right-angled triangle.");
+
+console.log("*");
+console.log("**");
+console.log("* *");
+console.log("*  *");
+console.log("*****");
+
+console.log("Use console.log() to create a zigzag pattern.");
+
+console.log("*     *");
+console.log(" *   * ");
+console.log("  * *  ");
+console.log("   *  ");
+console.log("  * *  ");
+console.log(" *   * ");
+console.log("*     *");
+
+console.log("all practise example done");
