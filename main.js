c7c2264335302a1a5bb1bb264b0e64564cbc9a87
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-10-11 12:38:47 +0500

    using console.log() to draw a 5x5 square
    Using console.log() to draw a 5x5 square done

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-console.log("Hi we are creating our first js file");
-console.log("good job");
+//console.log("Hi we are creating our first js file");
+//console.log("good job");
+
+console.log("Practice questions using only console.log()");
+
+console.log("Use console.log() to draw a 5x5 square of * symbols.");
+
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+
+
