26fe8722f9954ac90bd2bb88371935edd4579c21
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-10-11 13:07:32 +0500

    more examples,,,using console.log() done
    Use console.log() to draw a right-angled triangle made of *.
    Use console.log() to draw an inverted triangle.
    Create a pyramid using * symbols with 5 levels.
    Use console.log() to draw a diamond shape.
    Use console.log() to draw a 5x5 hollow square.
    Use console.log() to draw a hollow right-angled triangle.
    Use console.log() to create a zigzag pattern.

--- main.js
+++ main.js
@@ -11,4 +11,74 @@ console.log("*****");
 console.log("*****");
 console.log("*****");
 
+console.log("Use console.log() to draw a right-angled triangle made of *.");
 
+console.log("*");
+console.log("**");
+console.log("* *");
+console.log("*  *");
+console.log("*   *");
+console.log("*    *");
+console.log("*     *");
+console.log("*      *");
+console.log("*********");
+
+console.log("Use console.log() to draw an inverted triangle.");
+
+console.log("*********");
+console.log("*      *");
+console.log("*     *");
+console.log("*    *");
+console.log("*   *");
+console.log("*  *");
+console.log("* *");
+console.log("**");
+console.log("*");
+
+console.log("Create a pyramid using * symbols with 5 levels.");
+
+console.log("    *    ");
+console.log("   ***   ");
+console.log("  *****  ");
+console.log(" ******* ");
+console.log("*********");
+
+console.log("Use console.log() to draw a diamond shape.");
+
+console.log("    *    ");
+console.log("   ***   ");
+console.log("  *****  ");
+console.log(" ******* ");
+console.log("*********");
+console.log(" ******* ");
+console.log("  *****  ");
+console.log("   ***   ");
+console.log("    *    ");
+
+console.log("Use console.log() to draw a 5x5 hollow square.");
+
+console.log("*****");
+console.log("*   *");
+console.log("*   *");
+console.log("*   *");
+console.log("*****");
+
+console.log("Use console.log() to draw a hollow right-angled triangle.");
+
+console.log("*");
+console.log("**");
+console.log("* *");
+console.log("*  *");
+console.log("*****");
+
+console.log("Use console.log() to create a zigzag pattern.");
+
+console.log("*     *");
+console.log(" *   * ");
+console.log("  * *  ");
+console.log("   *  ");
+console.log("  * *  ");
+console.log(" *   * ");
+console.log("*     *");
+
+console.log("all practise example done");
