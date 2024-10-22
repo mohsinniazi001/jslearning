56a525958790a51b9ef714450d3c0ef91594acac
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-10-22 13:41:48 +0500

    string practice

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 //console.log("Hi we are creating our first js file");
 //console.log("good job");
 
+console.log("we are going to study string in js");
+
+let myname = "mohsin khan";
+
+console.log(myname);
+
+let brothername= "kamran khan";
+
+console.log("my name is"+ " "+ myname+ " " 
+    +"my bro name is "+ brothername);
+
