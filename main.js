fa71eec09683d52c9c47ec60e85e4f93d5268948
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-10-23 17:17:55 +0500

    more string practices

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ let myname = "mohsin khan";
 console.log(myname);
 
 let brothername= "kamran khan";
-
 let age = 7;
 
 console.log("my name is"+ " "+ myname+ " " 
     +"my bro name is "+ brothername + " " + "and his age is" +" "+ age );
 
+console.log(typeof(brothername));
+console.log(brothername.toUpperCase());
+
+console.log(brothername.toLowerCase());
+
+console.log(brothername.length);
+console.log(brothername[4]);
