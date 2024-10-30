a577d98afa6cf5b5f45e9aa9a59116022cf2612d
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-10-30 23:41:32 +0500

    done more practices of datatypes programs

--- datatypesPract.js
+++ datatypesPract.js
@@ -1,10 +1,4 @@
 // Number Data Type Practice Question
-// Write Program to convert feet to meter and meter into KM.
-// Write Program to convert celcius to farenheit. formula: (°C × 9/5) + 35
-// Write Program to convert farenheit to celcius. formula: (°F − 32) × 5/9
-// Write Program to Calculate Area of Circle. formula A=πr2
-// Write Program to Calculate Area of Square. formula A=a2
-// Write Program to Calculate Area of Rectangle. A=wl
 // Write Program to convert days to years and weeks
 console.log("Hey we are going to do  Number Data Type Practice Question");
 
@@ -68,14 +62,72 @@ console.log("the value of b :"+ b);
 console.log('swapping of three numbers');
 
 let x = 3 , y = 4 , z = 5;
-
-x = x - y + z;  ///  x=4
+console.log("Before Swapping values x = 3 , y = 4 , z = 5 ");
 
 z = x + y - z; ////z = 3
 
 y = x + y - z ; // y= 5
-
+console.log("After Swapping Values");
 console.log("the value of x is :"+ x);
 console.log("the value of y is :"+ y);
 console.log("the value of z is :"+ z);
+
+console.log("Write Program to convert feet to meter and meter into KM.");
+
+let feet = 20;
+let meter = 0;
+let Kilometer = 0;
+
+meter = feet * 0.3048;
+
+console.log("Then value of meter after conversion from " + feet + " feet is:" + meter + "meter");
+
+Kilometer = meter * 0.001;
+
+console.log("Then value of KM after conversion from " + meter + " meter is:" + Kilometer + "km");
+console.log("Write Program to convert celcius to farenheit. formula: (°C × 9/5) + 35");
+
+let celcius = 24;
+
+let farenheit = (celcius  *9/5) + 32;
+
+console.log(" The temperature from celcius to farhenite is " + farenheit);
+
+
+console.log("Write Program to convert farenheit to celcius. formula: (°F − 32) × 5/9");
+
+let F = 50;
+
+let cel = (F - 32) * 5/9;
+console.log(" The temperature from farhenite to celcius is " + cel);
+console.log("Write Program to Calculate Area of Circle. formula A=πr2");
+
+let area = 0;
+
+let radius = 8;
+
+let Pi = 3.14;
+
+area = Pi * radius*radius;
+
+console.log("Area=" + area);
+console.log("Write Program to Calculate Area of Square formula A=a2");
+
+area = 0;
+
+let squarevalue = 5;
+
+area = squarevalue* squarevalue;
+console.log("Area of Square is:" + area);
+
+console.log("// Write Program to Calculate Area of Rectangle. A=wl");
+
+let RectangleArea = 0;
+
+let widh = 100;
+let length= 40;
+RectangleArea = widh* length;
+
+console.log(" Area of Rectangle: " + RectangleArea);
+
 
