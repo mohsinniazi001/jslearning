ae33465c91c58075676358ced7b79e132f4b15f7
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-11-03 00:27:17 +0500

    Bool Relational and equility operators

--- arithmeticope.js
+++ arithmeticope.js
@@ -16,3 +16,18 @@ console.log("The result of Subtraction " + a + "-" + b + " is: " + sub);
 console.log("The result of Multiplication " + a + "*" + b + " is: " + mult);
 console.log("The result of Division " + a + "/" + b + " is: " + divide);
 console.log("The result of Modulus " + a + "%" + b + " is: " + modulus);
+
+
+console.log("Bool Relational and equility operators");
+
+let x = 20;
+let y = 30;
+
+console.log("Less than of " + "x = " + x + ", y = " + y + "is :" + x < y);
+console.log("Greater than of " + "x = " + x + ", y = " + y + "is :" + x > y);
+console.log("Less than or equal of " + "x = " + x + ", y = " + y + "is :" + x <= y);
+console.log("Greater than or Equal of " + "x = " + x + ", y = " + y + "is :" + x >= y);
+console.log("Equality of " + "x = " + x + ", y = " + y + "is :" + x == y);
+console.log("Equality of " + "x = " + x + ", y = " + y + "is :" + x == y);
+
+console.log("Not Equality of " + "x = " + x + ", y = " + y + "is :" + x != y);
