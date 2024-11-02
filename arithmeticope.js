09fb04fee53a68ce014d56a269c76f857b71627b
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-11-02 16:26:53 +0500

    Arithmetic operation example

---
+++ arithmeticope.js
@@ -0,0 +1,18 @@
+console.log('hey here you can find below example of all arithmetic operators');
+
+let a = 10;
+let b = 30;
+
+let add = a + b;
+let sub = a - b;
+let mult = a * b;
+let divide = a / b;
+let modulus = a % b;
+
+console.log("we are printing all values of above arithmetic operations");
+
+console.log("The result of Addition " + a + "+" + b + " is: " + add);
+console.log("The result of Subtraction " + a + "-" + b + " is: " + sub);
+console.log("The result of Multiplication " + a + "*" + b + " is: " + mult);
+console.log("The result of Division " + a + "/" + b + " is: " + divide);
+console.log("The result of Modulus " + a + "%" + b + " is: " + modulus);
