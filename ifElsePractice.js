675f0080c257c4b467e33b8abc0a978c401507d5
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-11-19 00:15:04 +0500

    get student marks and show grades programm
    Write a program that takes a student's score as input and assigns a letter grade based on the score

--- ifElsePractice.js
+++ ifElsePractice.js
@@ -31,3 +31,27 @@ else{
 
     console.log("Something went wrong");
 }
+
+console.log("Write a program that takes a student's score as input and assigns a letter grade based on the score");
+
+let marks = 0;
+marks = prompt("Enter the total marks of student: ");
+
+if( marks >= 90 || marks <= 100){
+    console.log("Grade of Student is: A");
+}
+
+else if(marks >= 80 || marks <= 89){
+
+    console.log("Grade of Student is: B");
+}
+else if(marks >= 70 || marks <= 79){
+
+    console.log("Grade of Student is: C");
+}
+else if(marks >= 60 || marks <= 69){
+
+    console.log("Grade of Student is: D");
+}else {
+    console.log("Grade of student is F")
+}
