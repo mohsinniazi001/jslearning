6ef4df41082d18949f4cd06dbe8e0a8f8185d74f
Mohsin Khan <mohsinniazi818839@gmail.com>
2024-11-19 21:29:34 +0500

    check the color of the traffic light

--- ifElsePractice.js
+++ ifElsePractice.js
@@ -34,8 +34,8 @@ else{
 
 console.log("Write a program that takes a student's score as input and assigns a letter grade based on the score");
 
-let marks = 0;
-marks = prompt("Enter the total marks of student: ");
+let marks = 92;
+// marks = prompt("Enter the total marks of student: ");
 
 if( marks >= 90 || marks <= 100){
     console.log("Grade of Student is: A");
@@ -54,4 +54,25 @@ else if(marks >= 60 || marks <= 69){
     console.log("Grade of Student is: D");
 }else {
     console.log("Grade of student is F")
+}
+
+console.log("Use conditions to check the color of the traffic light");
+// let green, yellow, red;
+let lightColor = "Green";
+// lightColor = prompt("Please enter Green, Yellow, or Red");
+
+if ( lightColor == "Green"){
+
+    console.log("hey dear Driver dont stop...GO");
+}else
+if ( lightColor == "Yellow"){
+
+    console.log("hey dear Driver plz slow down you car/bike");
+
+}else if ( lightColor == "Red"){
+
+    console.log("hey dear Driver STOP");
+}else 
+{
+    console.log("Invalid color! Please enter Green, Yellow, or Red.");
 }
